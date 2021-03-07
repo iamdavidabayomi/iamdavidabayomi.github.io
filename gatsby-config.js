4e34219e91d242bb70eaca6433450cb580240b0c
iamdavidabayomi <iamdavidabayomi@gmail.com>
2021-03-07 02:53:23 +0100

    first file commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Plugin used in gatsby here */
+  plugins: ["gatsby-plugin-postcss"],
+}
