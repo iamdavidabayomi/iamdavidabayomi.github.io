fbe3e8bcc4696ddb05ba6e0e8f7cb15b7366be6e
iamdavidabayomi <iamdavidabayomi@gmail.com>
2021-03-07 04:10:33 +0100

    fix bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,10 @@ export default function HomePage() {
   return (
     <>
       <div className="flex flex-col justify-center items-center text-center bg-gray-900 h-screen w-screen">
-        <div className="text-5xl text-white font-mono underline font-bold">
+        <div className=" text-xl md:text-5xl text-white font-mono underline font-bold">
           iamdavidabayomi
         </div>
-        <div className="text-3xl text-white font-mono font-medium italic">
+        <div className="text-base md:text-3xl text-white font-mono font-medium italic">
           website will be live soon!!!
         </div>
       </div>
