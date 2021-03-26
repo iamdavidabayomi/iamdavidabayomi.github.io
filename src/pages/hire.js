fa9bbb3c7fca19c23195ad7cb97e5add66e5a120
iamdavidabayomi <iamdavidabayomi@gmail.com>
2021-03-26 23:34:21 +0100

    add functionaity

---
+++ src/pages/hire.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const hire = () => {
+  return <div></div>
+}
+
+export default hire
---
+++ src/pages/shopify.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const shopify = () => {
+  return <div></div>
+}
+
+export default shopify
